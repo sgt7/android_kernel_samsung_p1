c86323a974ae9987639fe88cd731da35bde6ea45
Humberto Borba <humberos@gmail.com>
2012-01-03 09:40:40 -0200

    enviroment: remove ramdisk files to another repo - initramfs-cm9

--- clear-gitignore.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-find initramfs/ -name .gitignore > list
-while read file; do rm -Rf $file; done < list
-rm -Rf list
--- flash-heimdall.sh
+++
@@ -1,6 +0,0 @@
-#cp Kernel/arch/arm/boot/zImage Source/zImage
-#cd Source/
-#./flash.sh
-
-cp Kernel/arch/arm/boot/zImage .
-heimdall flash --kernel zImage
