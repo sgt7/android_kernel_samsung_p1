d5cdf7d154e3fc8a59f061b384329b26c73a03d7
Chirayu Desai <chirayudesai1@gmail.com>
2012-01-28 12:16:57 +0530

    updates for mtd
    Change-Id: Ia4edaa656ede9f304640170884a3506fbc3c37cd

--- build.sh
+++ build.sh
@@ -1,62 +1,117 @@
 #!/bin/bash
-#
-# Script to build Galaxy Tab Kernel
-# 2012 Chirayu Desai 
 
-# Common defines
-txtrst='\e[0m'  # Color off
-txtred='\e[0;31m' # Red
-txtgrn='\e[0;32m' # Green
-txtylw='\e[0;33m' # Yellow
-txtblu='\e[0;34m' # Blue
+setup ()
+{
+    if [ x = "x$ANDROID_BUILD_TOP" ] ; then
+        echo "Android build environment must be configured"
+        exit 1
+    fi
+    . "$ANDROID_BUILD_TOP"/build/envsetup.sh
 
-echo -e "${txtblu}##########################################"
-echo -e "${txtblu}#                                        #"
-echo -e "${txtblu}#      GALAXYTAB KERNEL BUILDSCRIPT      #"
-echo -e "${txtblu}#                                        #"
-echo -e "${txtblu}##########################################"
-echo -e "\r\n ${txtrst}"
+    KERNEL_DIR="$(dirname "$(readlink -f "$0")")"
+    BUILD_DIR="$KERNEL_DIR/build"
+    MODULES=("fs/cifs/cifs.ko" "fs/fuse/fuse.ko" "fs/nls/nls_utf8.ko")
 
-# Starting Timer
-START=$(date +%s)
-DEVICE="$1"
-THREADS=`cat /proc/cpuinfo | grep processor | wc -l`
+    if [ x = "x$NO_CCACHE" ] && ccache -V &>/dev/null ; then
+        CCACHE=ccache
+        CCACHE_BASEDIR="$KERNEL_DIR"
+        CCACHE_COMPRESS=1
+        CCACHE_DIR="$BUILD_DIR/.ccache"
+        export CCACHE_DIR CCACHE_COMPRESS CCACHE_BASEDIR
+    else
+        CCACHE=""
+    fi
 
-case "$DEVICE" in
-	clean)
-		make clean
-		exit
-		;;
-	p1|P1)
-		DEFCONFIG=p1_cm9_defconfig
-		;;
-	p1c|P1C)
-		DEFCONFIG=p1c_cm9_defconfig
-		;;
-	p1l|P1L)
-		DEFCONFIG=p1l_cm9_defconfig
-		;;
-	p1n|P1N)
-		DEFCONFIG=p1n_cm9_defconfig
-		;;
-	*)
-		echo -e "${txtred}Usage: $0 device"
-		echo -e "Example: ./build.sh p1"
-		echo -e "Supported Devices: p1 p1c p1l p1n ${txtrst}"
-		;;
-esac
+    CROSS_PREFIX="$ANDROID_BUILD_TOP/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-"
+}
 
+build ()
+{
+    local target=$1
+    echo "Building for $target"
+    local target_dir="$BUILD_DIR/$target"
+    local module
+    rm -fr "$target_dir"
+    mkdir -p "$target_dir/usr"
+    cp "$KERNEL_DIR/usr/"*.list "$target_dir/usr"
+    sed "s|usr/|$KERNEL_DIR/usr/|g" -i "$target_dir/usr/"*.list
+    mka -C "$KERNEL_DIR" O="$target_dir" ${target}_cm9_defconfig HOSTCC="$CCACHE gcc"
+    mka -C "$KERNEL_DIR" O="$target_dir" HOSTCC="$CCACHE gcc" CROSS_COMPILE="$CCACHE $CROSS_PREFIX" zImage modules
+    # cp "$target_dir"/arch/arm/boot/zImage $ANDROID_BUILD_TOP/device/samsung/galaxytab/kernel-$target
+    for module in "${MODULES[@]}" ; do
+        cp "$target_dir/$module" $ANDROID_BUILD_TOP/device/samsung/galaxytab/modules
+    done
+}
+    
+setup
 
-# The real build starts now
-if [ ! "$1" = "" ] ; then
-make -j$THREADS ARCH=arm $DEFCONFIG
-make -j$THREADS
+if [ "$1" = clean ] ; then
+    rm -fr "$BUILD_DIR"/*
+    exit 0
 fi
 
-# Copy Kernel to device
-cp arch/arm/boot/zImage ../../../device/samsung/galaxytab/kernel
+P1_target=$1
+targets=("$@")
+if [ 0 = "${#targets[@]}" ] ; then
+    targets=($P1_target)
+fi
 
-# The end!
+START=$(date +%s)
+
+for target in "${targets[@]}" ; do 
+    build $target
+done
+
+# Boot.img 
+KERNEL_DIR="$(dirname "$(readlink -f "$0")")"
+KERNEL_PATHS="$target_dir/arch/arm/boot/zImage"
+RECOVERY_INITRD=$ANDROID_BUILD_TOP/out/target/product/galaxytab/recovery/root
+KERNEL_INITRD=$ANDROID_BUILD_TOP/out/target/product/galaxytab/root
+
+PACKAGE_BOOTIMG()
+{
+	if [ "$1" = "" ] || [ "$2" = "" ]  || [ "$3" = "" ] ; then
+		ERROR_MSG="Error: PACKAGE_BOOTIMG - Missing args!"
+		return 1
+	fi
+	if [ ! -f "$1" ] ; then
+		ERROR_MSG="Error: PACKAGE_BOOTIMG - zImage does not exist!"
+		return 2
+	else
+		local KERNEL_INITRD="$2"
+		local RECOVERY_INITRD="$3"
+		echo "Creating ramdisk.img"
+		mkbootfs $KERNEL_INITRD | minigzip > ramdisk-kernel.img
+		echo "Creating ramdisk-recovery.img"
+		mkbootfs $RECOVERY_INITRD > ramdisk-recovery.cpio
+		minigzip < ramdisk-recovery.cpio > ramdisk-recovery.img
+		if [ -f boot.img ] ; then
+			echo "Deleting old boot.img"
+			rm -f $target_dir/boot.img
+		fi
+		echo "Creating boot.img"
+		./mkshbootimg.py $target_dir/boot.img $KERNEL_PATH ramdisk-kernel.img ramdisk-recovery.img
+		echo "Cleaning up temp files:"
+		echo "* rm -f ramdisk-kernel.img"
+		rm -f ramdisk-kernel.img
+		echo "* rm -f ramdisk-recovery.cpio"
+		rm -f ramdisk-recovery.cpio
+		echo "* rm -f ramdisk-recovery.img"
+		rm -f ramdisk-recovery.img
+	fi
+	return 0
+}
+
+PACKAGE_BOOTIMG "$KERNEL_PATH" "$KERNEL_INITRD" "$RECOVERY_INITRD"
+if [ $? != 0 ] ; then
+	echo "$ERROR_MSG"
+else
+	echo "boot.img created successfully"
+fi
+exit
+
+cp $target_dir/boot.img $ANDROID_BUILD_TOP//out/target/product/galaxytab/
+
 END=$(date +%s)
 ELAPSED=$((END - START))
 E_MIN=$((ELAPSED / 60))
