f86e7237e66d3e3fe82f4a4a9995b1781d96c48e
Chirayu Desai <chirayudesai1@gmail.com>
2012-01-30 14:56:45 +0530

    asagategagda
    Change-Id: Ie476d0d2bad31b8956ee0f8771a8c9890e821051

--- build.sh
+++ build.sh
@@ -46,78 +46,11 @@ case "$DEVICE" in
 		;;
 esac
 
-# Make it into a boot.img
-# ty nubecoder for this :)
-# defines
-KERNEL_DIR=`pwd`
-KERNEL_PATH="./arch/arm/boot/zImage"
-KERNEL_INITRD_DIR="../initramfs"
-KERNEL_INITRD_GIT="https://github.com/sgt7/p1000-initramfs-cm9.git"
-
-# Check if initramfs is present, if not, then clone it
-if [ ! -d $KERNEL_INITRD_DIR ]; then
-	cd ..
-	git clone $KERNEL_INITRD_GIT initramfs
-	cd $KERNEL_DIR
-fi
-
-# .git is huge!
-mv $KERNEL_INITRD_DIR/.git DONOTLOOKATME
-
 # The real build starts now
 if [ ! "$1" = "" ] ; then
-if [ "$3" = "boot.img" ] ; then
-sed -i "s|CONFIG_INITRAMFS_SOURCE="../initramfs"|CONFIG_INITRAMFS_SOURCE="usr/galaxytab_initramfs.list"|" arch/arm/configs/$DEFCONFIG
-else
-INITSOURCE=`grep CONFIG_INITRAMFS_SOURCE arch/arm/configs/$DEFCONFIG `
-if [ ! "$INITSOURCE" = "CONFIG_INITRAMFS_SOURCE="../initramfs"" ]; then
-sed -i "s|CONFIG_INITRAMFS_SOURCE="usr/galaxytab_initramfs.list"|CONFIG_INITRAMFS_SOURCE="../initramfs"|" arch/arm/configs/$DEFCONFIG
-fi
-fi
 make -j$THREADS ARCH=arm $DEFCONFIG
 make -j$THREADS
 fi
-
-# Function
-function PACKAGE_BOOTIMG()
-{
-	if [ "$1" = "" ] || [ "$2" = "" ] ; then
-		ERROR_MSG="Error: PACKAGE_BOOTIMG - Missing args!"
-		return 2
-	fi
-	if [ ! -f "$1" ] ; then
-		ERROR_MSG="Error: PACKAGE_BOOTIMG - zImage does not exist!"
-		return 1
-	else
-		echo -e "${txtblu} Creating ramdisk.img"
-		./tools/mkbootfs $KERNEL_INITRD_DIR | ./tools/minigzip > ramdisk.img
-		if [ -f boot.img ] ; then
-			echo "removing old boot.img"
-			rm -f boot.img
-		fi
-		echo -e "${txtblu} Creating boot.img"
-		./tools/mkshbootimg.py boot.img ./arch/arm/boot/zImage ramdisk.img
-		echo -e "${txtblu} Cleaning up temp files:"
-		rm -f ramdisk.img
-		echo -e "${txtblu} Done!"
-		return 0
-	fi
-}
-
-if [ "$3" = "boot.img" ] ; then
-# Main
-if [ ! "$1" = "" ] ; then
-PACKAGE_BOOTIMG "$KERNEL_PATH" "$KERNEL_INITRD_DIR"
-if [ $? != 0 ] ; then
-	echo -e "${txtred} $ERROR_MSG"
-else
-	echo -e "${txtblu} Boot.img created successfully...${txtrst}"
-fi
-fi
-fi
-
-# move it back just in case
-mv DONOTLOOKATME $KERNEL_INITRD_DIR/.git
 
 # The end!
 END=$(date +%s)
