49e16c31e928a8c49d4df69ac308386d59c3cfda
Chirayu Desai <chirayudesai1@gmail.com>
2012-02-19 11:55:00 +0530

    rename voodoo_sound* controls to wm8994_sound* allows control from CM9's built-in DSP Manager app
    Change-Id: I5f746e320b613bbe083071b417532a61fe666633

--- sound/soc/codecs/wm8994_voodoo.c
+++ sound/soc/codecs/wm8994_voodoo.c
@@ -1591,13 +1591,15 @@ static struct attribute_group voodoo_sound_control_group = {
 
 static struct miscdevice voodoo_sound_device = {
 	.minor = MISC_DYNAMIC_MINOR,
-	.name = "voodoo_sound",
+	//.name = "voodoo_sound",
+	.name = "wm8994_sound",
 };
 
 #ifndef MODULE
 static struct miscdevice voodoo_sound_control_device = {
 	.minor = MISC_DYNAMIC_MINOR,
-	.name = "voodoo_sound_control",
+	//.name = "voodoo_sound_control",
+	.name = "wm8994_sound_control",
 };
 #endif
 
