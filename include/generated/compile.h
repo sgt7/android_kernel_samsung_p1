e3d56e8c6b5795f66c7b010b762215ee731417ad
Chirayu Desai <chirayudesai1@gmail.com>
2012-03-09 17:26:07 +0530

    Compress p1 kernel with xz, saves space. Refactored code … …from crespo kernel-3.0.23.
    ====
    From 74374e4 Mon Sep 17 00:00:00 2001
    From: KalimochoAz <calimochoazucarado@gmail.com>
    Date: Thu, 1 Mar 2012 18:06:13 +0100
    Subject: [PATCH] Add compress xz possiblity for this kernel
    
    That will reduce size and will speed up loading
    
    From 24fa040 Mon Sep 17 00:00:00 2001
    From: Lasse Collin <lasse.collin@tukaani.org>
    Date: Wed, 12 Jan 2011 17:01:22 -0800
    Subject: [PATCH 1/2] decompressors: add XZ decompressor module
    
    In userspace, the .lzma format has become mostly a legacy file format that
    got superseded by the .xz format.  Similarly, LZMA Utils was superseded by
    XZ Utils.
    
    These patches add support for XZ decompression into the kernel.  Most of
    the code is as is from XZ Embedded <http://tukaani.org/xz/embedded.html>.
    It was written for the Linux kernel but is usable in other projects too.
    
    Advantages of XZ over the current LZMA code in the kernel:
      - Nice API that can be used by other kernel modules; it's
        not limited to kernel, initramfs, and initrd decompression.
      - Integrity check support (CRC32)
      - BCJ filters improve compression of executable code on
        certain architectures. These together with LZMA2 can
        produce a few percent smaller kernel or Squashfs images
        than plain LZMA without making the decompression slower.
    
    This patch: Add the main decompression code (xz_dec), testing module
    (xz_dec_test), wrapper script (xz_wrap.sh) for the xz command line tool,
    and documentation.  The xz_dec module is enough to have a usable XZ
    decompressor e.g.  for Squashfs.
    
    Signed-off-by: Lasse Collin <lasse.collin@tukaani.org>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Alain Knaff <alain@knaff.lu>
    Cc: Albin Tonnerre <albin.tonnerre@free-electrons.com>
    Cc: Phillip Lougher <phillip@lougher.demon.co.uk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    ====
    From 3ebe124 Mon Sep 17 00:00:00 2001
    From: Lasse Collin <lasse.collin@tukaani.org>
    Date: Wed, 12 Jan 2011 17:01:23 -0800
    Subject: [PATCH 2/2] decompressors: add boot-time XZ support
    
    This implements the API defined in <linux/decompress/generic.h> which is
    used for kernel, initramfs, and initrd decompression.  This patch together
    with the first patch is enough for XZ-compressed initramfs and initrd;
    XZ-compressed kernel will need arch-specific changes.
    
    The buffering requirements described in decompress_unxz.c are stricter
    than with gzip, so the relevant changes should be done to the
    arch-specific code when adding support for XZ-compressed kernel.
    Similarly, the heap size in arch-specific pre-boot code may need to be
    increased (30 KiB is enough).
    
    The XZ decompressor needs memmove(), memeq() (memcmp() == 0), and
    memzero() (memset(ptr, 0, size)), which aren't available in all
    arch-specific pre-boot environments.  I'm including simple versions in
    decompress_unxz.c, but a cleaner solution would naturally be nicer.
    
    Change-Id: I3104c2f0e8633e2067365ebb66c4158a254d916d
    Signed-off-by: Lasse Collin <lasse.collin@tukaani.org>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Alain Knaff <alain@knaff.lu>
    Cc: Albin Tonnerre <albin.tonnerre@free-electrons.com>
    Cc: Phillip Lougher <phillip@lougher.demon.co.uk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 20111216 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#20111216 PREEMPT Fri Dec 16 01:31:38 BRST 2011"
-#define LINUX_COMPILE_TIME "01:31:38"
-#define LINUX_COMPILE_BY "CM9-HumberOS"
-#define LINUX_COMPILE_HOST ""
-#define LINUX_COMPILE_DOMAIN "(none)"
-#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
