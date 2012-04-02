ac88045c192f93f28bd764509fb09721d7ac57cc
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-04-02 10:34:21 +0700

    mfc50: Adjust CPB_BUF_SIZE to prevent crash
    Memory adjustment in 2a8ea36ee305fcc01c417448e34a206dea7b00dd needs adjusted CPB_BUF_SIZE from the same source or MFC memory allocation will cause kernel panic

--- drivers/media/video/samsung/mfc50/mfc_memory.h
+++ drivers/media/video/samsung/mfc50/mfc_memory.h
@@ -66,7 +66,7 @@
 #define MFC_FW_BUF_SIZE       (512 * 1024)     /* 512KB : 512x1024 size per instance */
 
 #define RISC_BUF_SIZE         (0x80000)        /* 512KB : 512x1024 size per instance */
-#define CPB_BUF_SIZE          (0x300000)       /* 3MB   : 3x1024x1024 for decoder    */
+#define CPB_BUF_SIZE          (0x180000)       /* 1.5MB   : 1.5x1024x1024 for decoder    */
 #define DESC_BUF_SIZE         (0x20000)        /* 128KB : 128x1024                   */
 #define SHARED_BUF_SIZE       (0x10000)        /* 64KB  :  64x1024                   */
 #define PRED_BUF_SIZE         (0x10000)        /* 64KB  :  64x1024                   */
