1b4926d9851a0db936c2648144a588c59430f841
Mike Kasick <mike@kasick.org>
2012-05-09 23:47:38 -0300

    Call cpufreq_update_policy on DVFS events and force SLEEP_FREQ on suspend.
    Calls cpufreq_update_policy on DVFS lock/unlock events to ensure that DVFS
    locks take effect with the governor's preferred frequency (if higher than
    the lock frequency).  Previously the lock would set the frequency to the
    target level and expect the governor to reevaluate on a timer, which
    ondemand does, but powersave and performance do not.
    
    Forces the cpufreq policy, when suspending, to include SLEEP_FREQ, so as to
    prevent the device from crashing due to use of a "wrong" frequency.  Commit
    is based on [1], but includes additional locking to ensure policy isn't
    overwritten just prior to calling cpufreq_driver_target.  Also ensures that
    the user-specified cpufreq policy is reevaluated on resume, which is not
    done in the stock version.
    
    [1] https://github.com/nubecoder/nubernel-2.6.35/commit/6255b550af8c4f9d342ac4806e382dccfb40f4b6
    
    Change-Id: Ia50f1c9d1365ddfbcf4543f8acb4d00984284388

--- arch/arm/mach-s5pv210/cpu-freq.c
+++ arch/arm/mach-s5pv210/cpu-freq.c
@@ -314,29 +314,15 @@ static void s5pv210_cpufreq_clksrcs_APLL2MPLL(unsigned int index,
 #ifdef CONFIG_DVFS_LIMIT
 int s5pv210_lock_dvfs_high_level(uint nToken, uint perf_level)
 {
-	uint freq_level;
-	struct cpufreq_policy *cpu_policy = cpufreq_cpu_get(0);
-
-	if(cpu_policy ==NULL)
-	{
-		printk(KERN_WARNING "failed to modify the cpu frequency.\n");
-		return -EFAULT;
-	}
-	else
-	{
 #if 0 /* Remove debug message */
-		printk(KERN_DEBUG "%s : lock with token(%d) level(%d) current(%X)\n",
-				__func__, nToken, perf_level, g_dvfs_high_lock_token);
+	printk(KERN_DEBUG "%s : lock with token(%d) level(%d) current(%X)\n",
+			__func__, nToken, perf_level, g_dvfs_high_lock_token);
 #endif
-	}
 
 	if (g_dvfs_high_lock_token & (1 << nToken))
 		return 0;
 
 	if (perf_level > (MAX_PERF_LEVEL - 1))
-		return 0;
-
-	if (freq_table[perf_level].frequency > cpu_policy->max)
 		return 0;
 
 	//mutex_lock(&dvfs_high_lock);
@@ -349,9 +335,13 @@ int s5pv210_lock_dvfs_high_level(uint nToken, uint perf_level)
 
 	//mutex_unlock(&dvfs_high_lock);
 
-	freq_level = freq_table[perf_level].frequency;
+	/* Reevaluate cpufreq policy with the effect of calling the governor with a
+	 * CPUFREQ_GOV_LIMITS event, so that the governor sets its preferred
+	 * frequency.  The governor MUST call __cpufreq_driver_target, even if it
+	 * decides not to change frequencies, as the DVFS limit takes effect in
+	 * doing so. */
+	cpufreq_update_policy(0);
 
-	cpufreq_driver_target(cpu_policy, freq_level, CPUFREQ_RELATION_L);
 	return 0;
 }
 EXPORT_SYMBOL(s5pv210_lock_dvfs_high_level);
@@ -374,11 +364,15 @@ int s5pv210_unlock_dvfs_high_level(unsigned int nToken)
 	}
 
 	//mutex_unlock(&dvfs_high_lock);
-
 #if 0 /* Remove debug message */
 	printk(KERN_DEBUG "%s : unlock with token(%d) current(%X)\n",
 			__func__, nToken, g_dvfs_high_lock_token);
 #endif
+	*/
+	/* Reevaluate cpufreq policy with the effect of calling the governor with a
+	 * CPUFREQ_GOV_LIMITS event, so that the governor sets its preferred
+	 * frequency with the new (or no) DVFS limit. */
+	cpufreq_update_policy(0);
 
 	return 0;
 }
@@ -851,27 +845,41 @@ static int __init s5pv210_cpufreq_driver_init(struct cpufreq_policy *policy)
 static int s5pv210_cpufreq_notifier_event(struct notifier_block *this,
 		unsigned long event, void *ptr)
 {
-	static int max, min;
-	int ret;
-
-	struct cpufreq_policy *policy = cpufreq_cpu_get(0);
+	struct cpufreq_policy *policy;
+	int ret = -EINVAL;
 
 	switch (event) {
 	case PM_SUSPEND_PREPARE:
-		max = policy->max;
-		min = policy->min;
-		policy->max = policy->min = SLEEP_FREQ;
-		ret = cpufreq_driver_target(policy, SLEEP_FREQ,
+		if ((policy = cpufreq_cpu_get(0)) == NULL)
+			goto suspend_no_policy;
+		if (unlikely(lock_policy_rwsem_write(policy->cpu)))
+			goto suspend_lock_fail;
+
+		/* Ensure suspend policy includes SLEEP_FREQ, othewrise may crash. */
+		policy->min = policy->max = SLEEP_FREQ;
+
+		/* Call "internal" version as policy is already locked. */
+		ret = __cpufreq_driver_target(policy, SLEEP_FREQ,
 				DISABLE_FURTHER_CPUFREQ);
+
+		unlock_policy_rwsem_write(policy->cpu);
+suspend_lock_fail:
+		cpufreq_cpu_put(policy);
+suspend_no_policy:
 		if (ret < 0)
 			return NOTIFY_BAD;
 		return NOTIFY_OK;
 	case PM_POST_RESTORE:
 	case PM_POST_SUSPEND:
-		cpufreq_driver_target(policy, SLEEP_FREQ,
-				ENABLE_FURTHER_CPUFREQ);
-		policy->max = max;
-		policy->min = min;
+		if ((policy = cpufreq_cpu_get(0)) == NULL)
+			return NOTIFY_OK;
+		cpufreq_driver_target(policy, SLEEP_FREQ, ENABLE_FURTHER_CPUFREQ);
+		cpufreq_cpu_put(policy);
+
+		/* Reevaluate the user-specified cpufreq policy, which reverts the
+		 * forced SLEEP_FREQ used during suspend. */
+		cpufreq_update_policy(0);
+
 		return NOTIFY_OK;
 	}
 	return NOTIFY_DONE;
