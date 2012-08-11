fc02837b9934b73eeacb4c5986a5d1c518283771
humberos <humberos@gmail.com>
2012-08-11 20:32:46 -0300

    Update wireless driver: backport (read/copied) from Aries tree
    Change-Id: I72f90bcba96127b0dd434a95f5352bf6cb8e6a0b

--- drivers/net/wireless/bcm4329/include/wlioctl.h
+++ drivers/net/wireless/bcm4329/include/wlioctl.h
@@ -254,6 +254,11 @@ typedef struct wl_join_params {
 
 #define WLC_CNTRY_BUF_SZ	4		
 
+typedef struct wl_country {
+	char country_abbrev[WLC_CNTRY_BUF_SZ];
+	int32 rev;
+	char ccode[WLC_CNTRY_BUF_SZ];
+} wl_country_t;
 
 typedef enum sup_auth_status {
 	
@@ -857,7 +862,7 @@ typedef struct wl_ioctl {
 #define PM_MAX	1
 #define PM_FAST 2
 
-#define LISTEN_INTERVAL			20
+#define LISTEN_INTERVAL			10
 
 #define	INTERFERE_NONE	0	
 #define	NON_WLAN	1	
@@ -1342,12 +1347,6 @@ typedef struct wl_pfn {
 	int32			auth;			
 	uint32			wpa_auth;		
 	int32			wsec;			
-#ifdef WLPFN_AUTO_CONNECT
-	union {
-		wl_wsec_key_t	sec_key;		
-		wsec_pmk_t	wpa_sec_key;		
-	} pfn_security;
-#endif 
 } wl_pfn_t;
 
 #define PNO_SCAN_MAX_FW		508*1000
