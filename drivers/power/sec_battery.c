92ac0c526917a5d9296c35954aadb2c56af18bf5
humberos <humberos@gmail.com>
2012-09-28 00:32:41 -0300

    S5PC11X: BATTERY: allow multiple discharge reasons
    Change-Id: I6eeab2d3b11ca44b507c2c904d7dd6f16d7157a1
    Signed-off-by: Minsu Kim <minsu78.kim@samsung.com>

--- drivers/power/sec_battery.c
+++ drivers/power/sec_battery.c
@@ -696,23 +696,23 @@ static void sec_bat_discharge_reason(struct chg_data *chg)
 
 	discharge_reason = chg->bat_info.dis_reason & 0xf;
 
-	if (discharge_reason == DISCONNECT_BAT_FULL &&
+	if (discharge_reason & DISCONNECT_BAT_FULL &&
 			chg->bat_info.batt_vcell < RECHARGE_COND_VOLTAGE) {
 		chg->bat_info.dis_reason &= ~DISCONNECT_BAT_FULL;
 		chg->is_recharging = true;
 	}
 
-	if (discharge_reason == DISCONNECT_TEMP_OVERHEAT &&
+	if (discharge_reason & DISCONNECT_TEMP_OVERHEAT &&
 			chg->bat_info.batt_temp <=
 			HIGH_RECOVER_TEMP)
 		chg->bat_info.dis_reason &= ~DISCONNECT_TEMP_OVERHEAT;
 
-	if (discharge_reason == DISCONNECT_TEMP_FREEZE &&
+	if (discharge_reason & DISCONNECT_TEMP_FREEZE &&
 			chg->bat_info.batt_temp >=
 			LOW_RECOVER_TEMP)
 		chg->bat_info.dis_reason &= ~DISCONNECT_TEMP_FREEZE;
 
-	if (discharge_reason == DISCONNECT_OVER_TIME &&
+	if (discharge_reason & DISCONNECT_OVER_TIME &&
 			chg->bat_info.batt_vcell < RECHARGE_COND_VOLTAGE)
 		chg->bat_info.dis_reason &= ~DISCONNECT_OVER_TIME;
 
